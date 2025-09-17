530ef480b94eaa325df51fea4df5469166bc4f77
Gustavo <sousabarbosa9998@gmail.com>
2025-09-17 10:34:13 -0300

    Add files via upload

---
+++ analisador.texto.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome me letras maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em letras minúsculas é {}'.format(nome.lower()))
+print('No seu nome tem ao todo de {} letras'.format(len(nome)-nome.count(' ')))
+#print('Seu primero nome tem {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
